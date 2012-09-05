6b2046fab9654bf6dd71e006adca7189ae81fb42
Mike Slinn <mslinn@micronauticsresearch.com>
2012-09-05 16:13:44 -0700

    Folding in prototyped Java code

---
+++ app/com/micronautics/aws/bitBucket/BBDownloader.java
@@ -0,0 +1,39 @@
+package com.micronautics.aws.bitBucket;
+
+import org.apache.commons.io.FileUtils;
+import org.scribe.model.Response;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+/** Not needed any more because stream to stream copying is possible. */
+public class BBDownloader implements Callable<File> {
+    File tmpDir;
+    Commit commit;
+    String fileName;
+    BitBucketOAuth bitBucket;
+
+    public BBDownloader(File tmpDir, Commit commit, String fileName) throws IOException {
+        this.tmpDir = tmpDir;
+        this.commit = commit;
+        this.fileName = fileName;
+        bitBucket = new BitBucketOAuth();
+    }
+
+    public File call() {
+        try {
+            String urlStr = "https://bitbucket.org/" + commit.ownerName + "/" + commit.repoName + "/raw/master/" + fileName;
+            Response response = bitBucket.request(urlStr);
+            if (response.isSuccessful()) {
+                File file = new File(tmpDir, fileName);
+                FileUtils.copyInputStreamToFile(response.getStream(), file);
+                return file;
+            } else
+                return null;
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+            return null;
+        }
+    }
+}
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,29 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("AwsUpdate is ready."))
   }
   
+  def acceptBB = Action { implicit request =>
+	  request.body.asFormUrlEncoded match {
+	    case Some(data) =>
+	      data.get("payload") match {
+	      	case Some(payloadList) =>
+	      	  if (payloadList.size!=1) {
+	      	    BadRequest("payload list length was " + payloadList.size)
+              } else {
+  	            val payload = payloadList.head
+  	            val commit = com.micronautics.aws.bitBucket.JSON.parseCommit(payload)
+  	            val result = commit.repoName + "\n" + commit.ownerName + "\n"
+	            Ok(views.html.index("Got commit from BitBucket repo :" + result))
+	          }
+
+	        case _ =>
+	          BadRequest("payload list was empty")
+	      }
+
+	    case None =>
+	      BadRequest("No POST data found")
+	  }
+  }
 }
