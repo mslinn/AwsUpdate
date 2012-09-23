d778d7a750c7d9fd2d7c36f2be37f327f746cf4d
Mike Slinn <mslinn@mslinn.com>
2012-09-23 14:04:07 -0700

    Working on deep copy

--- app/com/micronautics/aws/bitBucket/BBCopy.scala
+++ app/com/micronautics/aws/bitBucket/BBCopy.scala
@@ -1,8 +1,6 @@
 package com.micronautics.aws.bitBucket
 
-import com.micronautics.aws.S3
 import java.io.File
-import java.io.IOException
 import java.util.concurrent.Callable
 import BBCopy._
 import model.Model._
@@ -24,14 +22,13 @@ object BBCopy {
 class BBCopy(val tmpDir: File, val commit: Commit, val fileName: String) extends Callable[Unit] {
 
     /** Read file corresponding to fileName from repository into a temporary file, stream to AWS S3, then delete temporary file */
+    // TODO use stream to stream copy instead
     def call(): Unit = {
         try {
-            //String urlStr = "https://bitbucket.org/" + commit.ownerName + "/" + commit.repoName + "/raw/master/" + fileName;
             val fileMetaJson: String = bitBucketBasicAuth.dirMetadata(commit.ownerName, commit.repoName, fileName)
             val fileSize: Int = JSON.parseFileSize(fileMetaJson, fileName)
             val rawFileUrl: String = bitBucketBasicAuth.urlStrRaw(commit.ownerName, commit.repoName, fileName)
-            val action: String = commit.filesToActions.get(fileName); // TODO figure out how to handle
-            //result += "  " + fileName + ": " + commit.filesToActions.get(fileName) + " " + fileSize + "bytes from " + rawFileUrl + "\n";
+            val action: String = commit.filesToActions.get(fileName); // TODO figure out how to handle various actions
             System.out.println("Copying '" + fileName + "' (" + fileSize + " bytes) to bucket '" + commit.repoName + "', owned by '" + commit.ownerName + "'")
             // TODO ensure bucket exists
             s3.uploadStream(commit.repoName.toLowerCase, fileName, bitBucketBasicAuth.getInputStream(rawFileUrl), fileSize)
